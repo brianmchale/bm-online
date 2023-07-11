64adb3ae5163359d91e780731e28e5affadc96ec
brianmchale <57236763+brianmchale@users.noreply.github.com>
2023-07-10 22:56:21 -0400

    Update gpttestportfolio.js

--- gpttestportfolio.js
+++ gpttestportfolio.js
@@ -1,9 +1,31 @@
 // script.js
 window.addEventListener('DOMContentLoaded', function() {
   var photographs = [
-    { src: '/Users/brianmchale/Pictures/2023/2023/2023-05-28/Edited/IMG_3098.jpg', title: 'Title 1', year: '2022' },
-    { src: '/Users/brianmchale/Pictures/2023/2023/2023-01-16/Edited/IMG_2577.jpg', title: 'Title 2', year: '2023' },
-    { src: 'path/to/image3.jpg', title: 'Title 3', year: '2021' },
+    { src: '86860006.JPG', title: 'Title 1', year: '2022' },
+    { src: '86860007.JPG', title: 'Title 2', year: '2023' },
+    { src: '86860009.JPG', title: 'Title 3', year: '2021' },
+    { src: '86860010.JPG', title: 'Title 1', year: '2022' },
+    { src: '86860019.JPG', title: 'Title 2', year: '2023' },
+    { src: '86860023.JPG', title: 'Title 3', year: '2021' },
+    { src: '86860027.JPG', title: 'Title 1', year: '2022' },
+    { src: '86860028.JPG', title: 'Title 2', year: '2023' },
+    { src: '86860030.JPG', title: 'Title 3', year: '2021' },
+    { src: '86860031.JPG', title: 'Title 1', year: '2022' },
+    { src: '86860034.JPG', title: 'Title 2', year: '2023' },
+    { src: '86860035.JPG', title: 'Title 3', year: '2021' },
+    { src: 'BIGGIE SMALLS.jpg', title: 'Title 1', year: '2022' },
+    { src: '86860017.JPG', title: 'Title 2', year: '2023' },
+    { src: 'IMG_0088.jpg', title: 'Title 3', year: '2021' },
+    { src: 'IMG_1378.jpg', title: 'Title 1', year: '2022' },
+    { src: 'IMG_1389.jpg', title: 'Title 2', year: '2023' },
+    { src: 'IMG_2563.jpg', title: 'Title 3', year: '2021' },
+    { src: 'IMG_2577.jpg', title: 'Title 1', year: '2022' },
+    { src: 'IMG_2797.jpg', title: 'Title 2', year: '2023' },
+    { src: 'IMG_2850.jpg', title: 'Title 3', year: '2021' },
+    { src: 'grafitti1.png', title: 'Title 3', year: '2021' },
+    { src: '23/25', title: 'Title 3', year: '2021' },
+    { src: '24/25', title: 'Title 3', year: '2021' },
+    { src: '25/25', title: 'Title 3', year: '2021' },
     // Add more photographs as needed
   ];
 
